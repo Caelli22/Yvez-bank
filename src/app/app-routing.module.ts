247da78b0e38b2244c4a728e3af19ea872e66596
Joshua Federico <joshuajay8848@gmail.com>
2023-11-16 14:13:52 +0800

    Log in page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,6 +14,10 @@ const routes: Routes = [
     path: 'sample',
     loadChildren: () => import('./sample/sample.module').then( m => m.SamplePageModule)
   },
+  {
+    path: 'login',
+    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
+  },
 
 ];
 

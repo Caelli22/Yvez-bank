908a04947e9289f1d194c8e6bf13e4cbb739582f
Elijah <joshuaelijahad@gmail.com>
2023-11-16 18:52:53 +0800

    g

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -18,6 +18,10 @@ const routes: Routes = [
     path: 'login',
     loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
   },
+  {
+    path: 'transfer',
+    loadChildren: () => import('./transfer/transfer.module').then( m => m.TransferPageModule)
+  },
 
 ];
 

2af0d70243ff8b6197c60235e03a0d65b583c69c
Elijah <joshuaelijahad@gmail.com>
2023-12-09 19:40:13 +0800

    Updated

--- src/app/transfer/transfer.page.ts
+++ src/app/transfer/transfer.page.ts
@@ -1,11 +1,21 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-transfer',
   templateUrl: './transfer.page.html',
   styleUrls: ['./transfer.page.scss'],
 })
 export class TransferPage implements OnInit {
+
+  constructor(private router: Router) { }
+
+  backToMain() {
+    this.router.navigate(['/main']);
+  }
+
+  gotoSend(){
+    this.router.navigate(['/transfer/send']);
+  }
 
   public profile: Array<any> = [
     { name: "Elijah Joshua A. Dimaculangan",
@@ -46,8 +56,6 @@ export class TransferPage implements OnInit {
   public onOther(){
   
   }
-
-  constructor() { }
 
   ngOnInit() {
   }
